fa9479cb91a25d9b8d5338c1a5e44567b644840b
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2021-07-10 08:42:35 -0600

    Agregando plantilla proyecto y archivo gitignore

---
+++ SOLID_Demo_Implementation/SOLID_Demo_Implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID_Demo_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
